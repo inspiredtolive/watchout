ced6f54e21b4d4d94acb469c8d26db2b69a9fdcf
Marco Chan <inspiredtolive0@gmail.com>
2016-08-05 15:31:54 -0700

    Add a player

--- client/watchout.js
+++ client/watchout.js
@@ -3,30 +3,45 @@ var width = +svg.attr('width');
 var height = +svg.attr('height');
 var data = [];
 for (var i = 0; i < 10; i++) {
-  data.push(i);
+  data.push('enemy');
 }
+data.push('player');
 
+// add data to circles
 var circles = svg.selectAll('circle')
     .data(data);
 
-circles.exit().remove();
-
+// add cirles to the dom as <circle>
 circles.enter().append('circle')
-    .attr('r', 10)
+    .attr('r', (d) => {
+      if (d === 'enemy') { return 15; }
+      return 10;
+    })
     .style('background-color', 'black')
-    .transition()
-    .attr('cx', () => Math.random() * 760 + 10)
-    .attr('cy', () => Math.random() * 480 + 10);
+    .style('fill', (d) => {
+      if (d === 'enemy') { return 'url(#image)'; }
+    })
+    .attr('class', (d) => d)
+    .attr('cx', () => Math.random() * 750 + 15)
+    .attr('cy', () => Math.random() * 470 + 15);
 
-var update = (data) => {
-
-  var circles = svg.selectAll('circle')
+// update enemy's position to random location
+var update = () => {
+  svg.selectAll('.enemy')
       .transition()
       .duration(1000)
-      .attr('cx', () => Math.random() * 760 + 10)
-      .attr('cy', () => Math.random() * 480 + 10);
+      .attr('cx', () => Math.random() * 750 + 15)
+      .attr('cy', () => Math.random() * 470 + 15);
 };
 
+// update player's position to cursor
+svg.on('mousemove', (e) => {
+  var player = svg.select('.player')
+      .attr('cx', d3.event.offsetX)
+      .attr('cy', d3.event.offsetY);
+});
+
+update();
 d3.interval(function() {
-  update(data);
+  update();
 }, 1000);
