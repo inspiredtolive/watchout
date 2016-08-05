4194c3b8b6bfa4cb3d5212c9365c1c3e5a7c49ec
Marco Chan <inspiredtolive0@gmail.com>
2016-08-05 12:35:07 -0700

    Draw circles and style board

--- client/watchout.js
+++ client/watchout.js
@@ -1,1 +1,32 @@
-// start slingin' some d3 here.
+var svg = d3.select('svg');
+var width = +svg.attr('width');
+var height = +svg.attr('height');
+var data = [];
+for (var i = 0; i < 10; i++) {
+  data.push(i);
+}
+
+var circles = svg.selectAll('circle')
+    .data(data);
+
+circles.exit().remove();
+
+circles.enter().append('circle')
+    .attr('r', 10)
+    .style('background-color', 'black')
+    .transition()
+    .attr('cx', () => Math.random() * 760 + 10)
+    .attr('cy', () => Math.random() * 480 + 10);
+
+var update = (data) => {
+
+  var circles = svg.selectAll('circle')
+      .transition()
+      .duration(1000)
+      .attr('cx', () => Math.random() * 760 + 10)
+      .attr('cy', () => Math.random() * 480 + 10);
+};
+
+d3.interval(function() {
+  update(data);
+}, 1000);
