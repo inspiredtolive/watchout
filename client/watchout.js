6bb8a36bcf45f6ef562ef7da02f439d49fcbb48f
Marco Chan <inspiredtolive0@gmail.com>
2016-08-06 15:05:30 -0700

    Improve enemy movement

--- client/watchout.js
+++ client/watchout.js
@@ -34,13 +34,28 @@ var update = () => {
   enemies.transition()
       .duration(1000)
       .attr('cx', () => Math.random() * 750 + 15)
-      .attr('cy', () => Math.random() * 470 + 15);
+      .attr('cy', () => Math.random() * 470 + 15)
+      .on('end', update);
 };
 
 // update player's position to cursor
 svg.on('mousemove', () => {
-  player.attr('cx', d3.event.offsetX)
-      .attr('cy', d3.event.offsetY);
+  var xOffset = d3.event.offsetX;
+  var yOffset = d3.event.offsetY;
+  if ( xOffset < 15) {
+    xOffset = 15;
+  }
+  if ( yOffset < 15) {
+    yOffset = 15;
+  }
+  if ( xOffset > 785) {
+    xOffset = 785;
+  }
+  if ( yOffset > 485) {
+    yOffset = 485;
+  }
+  player.attr('cx', xOffset)
+      .attr('cy', yOffset);
 });
 var checkCollision = function () {
   svg.style('background-color', '#eee');
@@ -53,7 +68,7 @@ var checkCollision = function () {
     var xDiff = thisEnemy.attr('cx') - player.attr('cx');
     var yDiff = thisEnemy.attr('cy') - player.attr('cy');
     var dist = Math.sqrt(Math.pow(xDiff, 2) + Math.pow(yDiff, 2));
-    if (dist <= 15) {
+    if (dist <= 30) {
       collided = true;
     }
   });
@@ -61,19 +76,19 @@ var checkCollision = function () {
 
   if (collided) {
     player.transition()
-      .duration(125)
+      .duration(200)
       .attr('opacity', 0.3)
       .transition()
-      .duration(125)
+      .duration(200)
       .attr('opacity', 1)
       .transition()
-      .duration(125)
+      .duration(200)
       .attr('opacity', 0.3)
       .transition()
-      .duration(125)
+      .duration(200)
       .attr('opacity', 1);
 
-    delay = 500;
+    delay = 800;
     svg.style('background-color', 'red');
     if (highScore < currentScore) {
       highScore = currentScore;
@@ -92,7 +107,6 @@ d3.timeout(checkCollision, 10 + delay);
 
 update();
 d3.interval(function() {
-  update();
   currentScore += 1;
   d3.select('.current span').text(currentScore);
-}, 1000);
+}, 500);
